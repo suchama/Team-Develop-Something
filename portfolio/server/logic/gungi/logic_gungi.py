8969d9c9390d2009655c330c21bb01afcfbf27b6
sattyan1234 <161127976+sattyan1234@users.noreply.github.com>
2025-11-29 20:08:44 +0900

    a

--- portfolio/server/logic/gungi/logic_gungi.py
+++ portfolio/server/logic/gungi/logic_gungi.py
@@ -289,9 +289,7 @@ def handle_bou( board,
     }
 
 
-def handle_ai_move(board,
-                   tegoma,
-                   gamestate_dict,
+def handle_ai_move(gamestate_dict,
                    current_turn,
                    ) -> Dict:
     """
@@ -300,8 +298,10 @@ def handle_ai_move(board,
     - 手駒を使うことも考慮（自陣の空きマスへランダム配置）。
     """
     b = Board()
+    board = gamestate_dict["board"]
     b.grid = [[layer[:] for layer in row] for row in board]
     gs = GameState()
+    tegoma = gamestate_dict["tegoma"]
     gs.hands = {1: dict(tegoma[1]), 2: dict(tegoma[2])}
     gs.board = b
 
